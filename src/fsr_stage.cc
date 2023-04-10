f400ddb68516c29bed265e0c51ea3950c5613082
Erfan Momeni <erfanmo98@gmail.com>
2023-04-10 14:36:49 +0300

    fixed fsr headers

--- src/fsr_stage.cc
+++ src/fsr_stage.cc
@@ -1,26 +1,6 @@
 #include "fsr_stage.hh"
-#include "scene.hh"
-#include "misc.hh"
 
 namespace
 {
-using namespace tr;
-
-
-
-
-void direct_stage::init_scene_resources()
-{
-   
-}
-
-void direct_stage::record_command_buffer_pass(
-    vk::CommandBuffer cb,
-    uint32_t frame_index,
-    uint32_t pass_index,
-    uvec3 expected_dispatch_size
-){
-}
-
 }
 
--- src/fsr_stage.hh
+++ src/fsr_stage.hh
@@ -1,44 +1,10 @@
 #ifndef TAURAY_FSR_STAGE_HH
 #define TAURAY_FSR_STAGE_HH
 #include "context.hh"
-#include "texture.hh"
-#include "stage.hh"
-#include "compute_pipeline.hh"
-#include "timer.hh"
-#include "gbuffer.hh"
-#include "gpu_buffer.hh"
-#include "scene.hh"
-#include "sampler.hh"
+#include <FSR/ffx_fsr2.h>
 
 namespace tr
 {
-
-class scene;
-class fsr_stage: public stage
-{
-public:
-
-    fsr_stage(
-        device_data& dev,
-        uint32_t frame_index,
-        uint32_t pass_index,
-        uvec3 expected_dispatch_size
-    );
-
-protected:
-    void init_scene_resources() override;
-    void record_command_buffer_pass(
-        vk::CommandBuffer cb,
-        uint32_t frame_index,
-        uint32_t pass_index,
-        uvec3 expected_dispatch_size
-    ) override;
-
-private:
-    gfx_pipeline gfx;
-    options opt;
-};
-
 }
 
 #endif
